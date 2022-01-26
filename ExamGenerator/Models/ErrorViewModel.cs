1462549671478ae01748bbafa34566b04be434dd
Ozden <Ozden@DESKTOP-OLLPLDV>
2022-01-26 03:20:51 +0300

    Add project files.

---
+++ ExamGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
