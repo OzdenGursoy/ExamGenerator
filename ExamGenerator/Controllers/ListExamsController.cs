d0ee0a5173a15923fbedc15555d7e1a244cf00d7
OzdenGursoy <ogursoy15@ku.edu.tr>
2022-02-03 03:33:17 +0300

    Implemented Delete Exam

--- ExamGenerator/Controllers/ListExamsController.cs
+++ ExamGenerator/Controllers/ListExamsController.cs
@@ -21,5 +21,22 @@ namespace ExamGenerator.Controllers
             var data = _context.Exam.ToList();
             return View(data);
         }
+
+        public IActionResult DeleteExam(int id)
+        {
+            var questions = _context.Question.Where(obj => obj.ExamId == id).Select(col => col.QId);
+            foreach (var question in questions.ToList())
+            {
+                var row = _context.Question.Find(question);
+                _context.Question.Remove(row);
+                _context.SaveChanges();
+            }
+
+            var exam = _context.Exam.Find(id);
+            _context.Exam.Remove(exam);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
