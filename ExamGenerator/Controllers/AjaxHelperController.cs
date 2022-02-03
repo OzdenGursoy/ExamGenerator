ed98ddc5140896e50c4fdcd6d65ddfc9c2955b25
OzdenGursoy <ogursoy15@ku.edu.tr>
2022-02-03 11:25:16 +0300

    Imlemented Take Exam page

---
+++ ExamGenerator/Controllers/AjaxHelperController.cs
@@ -0,0 +1,31 @@
+﻿using ExamGenerator.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamGenerator.Controllers
+{
+    public class AjaxHelperController : Controller
+    {
+        private readonly ExamGeneratorDbContext _context;
+
+        public AjaxHelperController(ExamGeneratorDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult getColorList(int id,string op1, string op2, string op3, string op4)
+        {
+            var questions = _context.Question.Where(obj => obj.ExamId == id).ToList();
+
+            List<string> colorList = new List<string>();
+
+            colorList.Add((op1 == questions[0].CorrectOp) ? "green" : "red");
+            colorList.Add((op2 == questions[1].CorrectOp) ? "green" : "red");
+            colorList.Add((op3 == questions[2].CorrectOp) ? "green" : "red");
+            colorList.Add((op4 == questions[3].CorrectOp) ? "green" : "red");
+
+            return Json(colorList);
+        }
+    }
+}
+    
---
+++ ExamGenerator/Controllers/ExamController.cs
@@ -0,0 +1,28 @@
+﻿using ExamGenerator.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamGenerator.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly ExamGeneratorDbContext _context;
+
+        public ExamController(ExamGeneratorDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult TakeExam(int id)
+        {
+            var questions = _context.Question.Where(obj => obj.ExamId == id).ToList();
+            var exam = _context.Exam.Find(id);
+
+            ViewBag.q1 = questions[0];
+            ViewBag.q2 = questions[1];
+            ViewBag.q3 = questions[2];
+            ViewBag.q4 = questions[3];
+
+            return View(exam);
+        }
+    }
+}
